264cf4541a488bd9ae5e457e9654db9272585823
wkirk01 <30280510+wkirk01@users.noreply.github.com>
2018-12-11 00:48:03 -0600

    Minor performance updates

--- src/app/toolbar/list-menu/list-menu.component.ts
+++ src/app/toolbar/list-menu/list-menu.component.ts
@@ -7,7 +7,6 @@ import { list } from 'src/models/list';
 import { ListManagerComponent } from '../../list-manager/list-manager.component'
 import { MatBottomSheet } from '@angular/material';
 
-
 @Component({
   selector: 'app-list-menu',
   templateUrl: './list-menu.component.html',
@@ -17,20 +16,29 @@ export class ListMenuComponent implements OnChanges {
 
   @Input() user: user;
 
-  friends: Observable<user[]>;
+  friends: user[];
 
-  lists: Observable<list[]>;
+  lists: list[];
 
   constructor(private database: ItemsService, private auth: AuthService, private bottomSheet: MatBottomSheet) { }
 
   ngOnChanges(changes: SimpleChanges) {
     //check if user exists to prevent logout error
-    if (!changes.user.currentValue) {
+    if (!changes.user) {
       return
     }
-    if (!changes.user.previousValue || changes.user.currentValue.currentList != changes.user.previousValue.currentList) {
-      this.lists = this.database.getLists(this.user);
-      this.friends = this.auth.getFriends(this.user);
+    if (changes.user.currentValue && !changes.user.previousValue) {
+      this.database.getLists(this.user).subscribe((lists) => {
+        this.lists = lists;
+      })
+      this.auth.getFriends(this.user).subscribe((friends) => {
+        this.friends = friends;
+      })
+    }
+    if (changes.user.currentValue && changes.user.previousValue && changes.user.currentValue.currentList != changes.user.previousValue.currentList) {
+      this.database.getLists(this.user).subscribe((lists) => {
+        this.lists = lists;
+      })
     }
   }
 
